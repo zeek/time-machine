e1dd50d82dbac6b25c70da3fce3b62c02bb005a1
Gregor Maier <gregor@icir.org>
2011-07-22 13:32:29 -0700

    Fixing autogen.sh which runs the autotools toolchain.

--- autogen.sh
+++ autogen.sh
@@ -1,24 +1,26 @@
 
 # $Id: autogen.sh 161 2006-12-19 02:35:21Z gregor $
 
-if which aclocal-1.9 > /dev/null 2>/dev/null; then
-	ACLOCAL=aclocal-1.9
-elif which aclocal19 > /dev/null 2>/dev/null; then
-	ACLOCAL=aclocal19
-else 
-	echo "Could not found aclocal-1.9 or aclocal19. Exiting"
-	exit
-fi
+aclocal && autoheader && autoconf && automake --add-missing --copy
 
-if which  automake-1.9 > /dev/null 2>/dev/null; then
-	AM=automake-1.9
-elif which  automake19 > /dev/null 2>/dev/null ; then
-	AM=automake19
-else 
-	echo "Could not found automake-1.9 or automake19. Exiting"
-	exit
-fi
-
+#if which aclocal-1.9 > /dev/null 2>/dev/null; then
+#	ACLOCAL=aclocal-1.9
+#elif which aclocal19 > /dev/null 2>/dev/null; then
+#	ACLOCAL=aclocal19
+#else 
+#	echo "Could not found aclocal-1.9 or aclocal19. Exiting"
+#	exit
+#fi
+#
+#if which  automake-1.9 > /dev/null 2>/dev/null; then
+#	AM=automake-1.9
+#elif which  automake19 > /dev/null 2>/dev/null ; then
+#	AM=automake19
+#else 
+#	echo "Could not found automake-1.9 or automake19. Exiting"
+#	exit
+#fi
+#
 
 #aclocal-1.9 && autoheader && autoconf && automake-1.9 --add-missing --copy
 
@@ -26,7 +28,7 @@ fi
 # autoconf    depends on aclocal
 # configure depends on everything
 # no other dependencies
-${ACLOCAL} \
-	&& autoheader \
-	&&  autoconf \
-	&& ${AM} --add-missing --copy
+#${ACLOCAL} \
+#	&& autoheader \
+#	&&  autoconf \
+#	&& ${AM} --add-missing --copy
